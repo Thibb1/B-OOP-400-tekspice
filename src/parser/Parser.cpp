2ca7e7a4e4e93d818fca584f74e64a718e6b0835
Hugo Delille <hugo.delille@epitech.eu>
2022-02-04 14:04:43 +0100

    +parsercpp and parser.hpp and file repo

---
+++ src/parser/Parser.cpp
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT, 2022
+** /home/hugo/Epitach/tekspice
+** File description:
+** RegUtil.cpp
+*/
+
+void parser()
+{
+    }
---
+++ src/parser/Parser.hpp
@@ -0,0 +1,6 @@
+/*
+** EPITECH PROJECT, 2022
+** /home/hugo/Epitech/tekspice
+** File description:
+** RegUtil.hpp
+*/
